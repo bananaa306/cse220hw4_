22e60ca30659d0e652a9cce5f962f7b56060bb1f
Wilson <wilson.tang.1@stonybrook.edu>
2026-03-14 03:58:16 +0000

    finish

--- cse_caesar.c
+++ cse_caesar.c
@@ -3,17 +3,81 @@
 /* DO NOT use <string.h> or <ctype.h> */
 
 int encryptCaesar(const char *plaintext, char *ciphertext, int key) {
-    (void)plaintext;
-    (void)ciphertext;
-    (void)key;
-    /* TODO */
-    return 0;
+    const char marker[] = "__EOM__";
+    int marker_len = 7;
+    if (plaintext == NULL || ciphertext == NULL) {
+        return -2;
+    }
+    if (strgLen(ciphertext) < marker_len) {
+        return -1;
+    }
+    int max_payload = strgLen(ciphertext) - marker_len;
+    int encoded = 0;
+    while (plaintext[encoded] != '\0' && encoded < max_payload) {
+        char c = plaintext[encoded];
+        int shift = key + encoded;
+        if (c >= 'a' && c <= 'z') {
+            int off = (c - 'a' + shift) % 26;
+            if (off < 0) {
+                off += 26;
+            }
+            ciphertext[encoded] = (char)('a' + off);
+        } else if (c >= 'A' && c <= 'Z') {
+            int off = (c - 'A' + shift) % 26;
+            if (off < 0) {
+                off += 26;
+            }
+            ciphertext[encoded] = (char)('A' + off);
+        } else {
+            ciphertext[encoded] = c;
+        }
+        encoded++;
+    }
+    for (int i = 0; i < marker_len; i++) {
+        ciphertext[encoded + i] = marker[i];
+    }
+    ciphertext[encoded + marker_len] = '\0';
+    return encoded;
 }
 
 int decryptCaesar(const char *ciphertext, char *plaintext, int key) {
-    (void)ciphertext;
-    (void)plaintext;
-    (void)key;
-    /* TODO */
-    return 0;
+    const char marker[] = "__EOM__";
+    int marker_len = 7;
+    if (ciphertext == NULL || plaintext == NULL) {
+        return -2;
+    }
+    int i = 0;
+    while (ciphertext[i] != '\0') {
+        int is_marker = 1;
+        for (int j = 0; j < marker_len; j++) {
+            if (ciphertext[i + j] == '\0' || ciphertext[i + j] != marker[j]) {
+                is_marker = 0;
+                break;
+            }
+        }
+        if (is_marker) {
+            plaintext[i] = '\0';
+            return i;
+        }
+        char c = ciphertext[i];
+        int shift = key + i;
+        if (c >= 'a' && c <= 'z') {
+ 
+            int off = (c - 'a' - shift) % 26;
+            if (off < 0) {
+                off += 26;
+            }
+            plaintext[i] = (char)('a' + off);
+        } else if (c >= 'A' && c <= 'Z') {
+            int off = (c - 'A' - shift) % 26;
+            if (off < 0) {
+                off += 26;
+            }
+            plaintext[i] = (char)('A' + off);
+        } else {
+            plaintext[i] = c;
+        }
+        i++;
+    }
+    return -1;
 }
--- strgPtr.c
+++ strgPtr.c
@@ -4,7 +4,16 @@
 int strgLen(const char *s) {
     (void)s;
     /* TODO */
-    return 0;
+    size_t count=0;
+    if(s == NULL)
+    {
+        return -1;
+    }
+    for(int i=0; s[i]!=NULL;i++)
+    {
+        count++;
+    }
+    return count;
 }
 
 void strgCopy(const char *source, char *destination, size_t size) {
@@ -12,29 +21,128 @@ void strgCopy(const char *source, char *destination, size_t size) {
     (void)destination;
     (void)size;
     /* TODO */
+    if(source == NULL || destination== NULL)
+    {
+        return;
+    }
+    if(size ==0)
+    {
+        return;
+    }
+    if(size ==1 )
+    {
+        destination[0] = '\0';
+        return;
+    }
+    int i=0;
+    for(i; i<size-1;i++)
+    {
+        if(source[i] == NULL)
+        {
+            destination[i]= '\0';
+            return;
+        }
+        
+        destination[i] = source[i];
+        
+    }
+    destination[i] = '\0';
+    return;
 }
 
 void strgChangeCase(char *s) {
     (void)s;
     /* TODO */
+    if(s==NULL) return;
+    int pos=0;
+    for(int i=0; s[i] != '\0'; i++)
+    {
+        if( s[i] >='a' && s[i]<='z')
+        {
+            if( i==0 && (s[i+1] >='0' && s[i+1]<='9'))
+            {
+                continue;
+            }
+            else if(i == strgLen(s)-1 && (s[i+1] >='0' && s[i+1]<='9')){
+                continue;
+            }
+            else if( (s[i+1] >='0' && s[i+1]<='9') || (s[i-1] >='0' && s[i-1]<='9'))
+            {
+                continue;
+            }
+            else{
+                s[i] -=32; 
+            }
+        }
+        else if(s[i] >='A' && s[i]<='Z')
+        {
+            if( i==0 && (s[i+1] >='0' && s[i+1]<='9'))
+            {
+                continue;
+            }
+            else if(i == strgLen(s)-1 && (s[i+1] >='0' && s[i+1]<='9')){
+                continue;
+            }
+            else if( (s[i+1] >='0' && s[i+1]<='9') || (s[i-1] >='0' && s[i-1]<='9'))
+            {
+                continue;
+            }
+            else{
+                s[i] +=32; 
+            }
+        }
+    }
+
 }
 
 int strgDiff(const char *s1, const char *s2) {
     (void)s1;
     (void)s2;
     /* TODO */
-    return 0;
+    if(s1 == NULL || s2==NULL ) return -2;
+    int i=0;
+    for(i; s1[i] != '\0' || s2[i] != '\0';i++)
+    {
+        if(s1[i] != s2[i])
+        {
+            return i;
+        }
+    }
+    return -1;
+    
 }
-
 void strgInterleave(const char *s1, const char *s2, char *d, size_t size) {
     (void)s1;
     (void)s2;
     (void)d;
     (void)size;
     /* TODO */
+    if(s1 == NULL || s2 == NULL || d == NULL || size == NULL) return;
+    size_t i=0;
+    size_t is1 = 0;
+    size_t is2 =0; 
+    while (i<size -1 && (s1[is1] != '\0' || s2[is2] != '\0' )){
+        if(i<size-1 && (s1[is1] != '\0')){
+            d[i++] = s1[is1++];
+        }
+        if(i<size-1 && s2[is2] != '\0'){
+            d[i++] = s2[is2++];
+        }
+    }
+    d[i]='\0';
 }
 
 void strgReverseLetters(char *s) {
-    (void)s;
-    /* TODO */
+    if (s == NULL) {
+        return;
+    }
+    int left = 0;
+    int right = strgLen(s) - 1;
+    while (left < right) {
+        char temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+        left++;
+        right--;
+    }
 }
